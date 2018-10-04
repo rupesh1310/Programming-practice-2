6ce94fee55f8a980c12abb6f6dc8440183aa7531
A.Rupesh <a.rupesh66@gmail.com>
2018-10-05 02:01:27 +0530

    updated chapter 2 and added binarysearch recursion

--- chapter 2/programming practice/HighArrayApp.java
+++ chapter 2/programming practice/HighArrayApp.java
@@ -1,4 +1,25 @@
+//1.To the HighArray class in the highArray.java program (Listing 2.3), add a
+// method called getMax() that returns the value of the highest key in the array,
+// or –1 if the array is empty. Add some code in main() to exercise this method.
+// You can assume all the keys are positive numbers.
 
+//2.Modify the method in Programming Project 2.1 so that the item with the
+// highest key is not only returned by the method, but also removed from the
+// array. Call the method removeMax()
+
+//3.The removeMax() method in Programming Project 2.2 suggests a way to sort
+// the contents of an array by key value. Implement a sorting scheme that does
+// not require modifying the HighArray class, but only the code in main(). You’ll
+// need a second array, which will end up inversely sorted. (This scheme is a
+// rather crude variant of the selection sort in Chapter 3, “Simple Sorting.”)
+
+//6.Write a noDups() method for the HighArray class of the highArray.java
+// program (Listing 2.3). This method should remove all duplicates from the
+// array. That is, if three items with the key 17 appear in the array, noDups()
+// should remove two of them. Don’t worry about maintaining the order of the
+// items. One approach is to first compare every item with all the other items and
+// overwrite any duplicates with a null (or a distinctive value that isn’t used for
+// real keys). Then remove all the nulls. Of course, the array size will be reduced.
 class HighArray
    {
    private long[] a;                 
--- chapter 2/programming practice/OrderedApp.java
+++ chapter 2/programming practice/OrderedApp.java
@@ -1,4 +1,16 @@
+//4.Modify the orderedArray.java program (Listing 2.4) so that the insert() and
+// delete() routines, as well as find(), use a binary search, as suggested in the
+// text.
 
+//5.Add a merge() method to the OrdArray class in the orderedArray.java
+// program (Listing 2.4) so that you can merge two ordered source arrays into an
+// ordered destination array. Write code in main() that inserts some random
+// numbers into the two source arrays, invokes merge(), and displays the contents
+// of the resulting destination array. The source arrays may hold different
+// numbers of data items. In your algorithm you will need to compare the keys of
+// the source arrays, picking the smallest one to copy to the destination. You’ll
+// also need to handle the situation when one source array exhausts its contents
+// before the other.
 class OrdArray
    {
    private long[] a;                 
